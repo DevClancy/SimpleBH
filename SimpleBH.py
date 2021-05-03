90933760df6cdb7399d733b2374f383255e33db7
DevClancy <70335809+DevClancy@users.noreply.github.com>
2021-05-03 16:01:25 +0300

    Create SimpleBH.py

---
+++ SimpleBH.py
@@ -0,0 +1,44 @@
+import time
+import os
+from threading import Thread
+import pymem
+from pymem import process
+import pymem.process
+import requests
+import keyboard
+from colorama import Fore
+
+init()
+
+offsets = 'https://raw.githubusercontent.com/kadeeq/ProjectX/main/offsets/offsets.json'
+response = requests.get( offsets ).json()
+
+dwForceJump = int( response["signatures"]["dwForceJump"] )
+dwLocalPlayer = int( response["signatures"]["dwLocalPlayer"] )
+m_fFlags = int( response["netvars"]["m_fFlags"] )
+
+
+def bhop():
+	try:
+		pm = pymem.Pymem("csgo.exe")
+		client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
+		engine = pymem.process.module_from_name(pm.process_handle, "engine.dll").lpBaseOfDll
+
+		os.system("cls")
+
+		print(Fore.CAYN + "Author: Clancy \nYouTube: https://www.youtube.com/c/ClancyCheats \nDonate: QIWI: DEVCLANCY \nDonationAlerts: https://www.donationalerts.com/r/theclancy")
+
+		while True:
+			player = pm.read_int(client + dwLocalPlayer)
+			if keyboard.is_pressed( "space" ):
+				force_jump = client + dwForceJump
+				on_ground = pm.read_int( player + m_fFlags )
+				if player and on_ground == 257 or on_ground == 263:
+					pm.write_int( force_jump, 6 )
+	except:
+		print("[SimpleBH] Откройте CSGO!")
+		time.sleep(0.3)
+
+
+main = Thread(target = bhop)
+main.start()
